af077ce7d07f9f0a5e28b1390eb981254893c02c
EC2 Default User <ec2-user@ip-172-31-70-32.ec2.internal>
2021-06-15 08:01:36 +0000

    [Finish] Rails-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b4c1ef8b0c237207a44204f7dc5205d243ee72a5dd91395acd5646d7c8783c071fa8052119a2516680df1e7b857a528ec3a076b095dac2f841db9025ae1defa8'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_13_102421) do
+ActiveRecord::Schema.define(version: 2021_06_15_064457) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_06_13_102421) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "body"
+    t.string "image_id"
   end
 
 end
