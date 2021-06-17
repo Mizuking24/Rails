44f0ab6b06f9a3c21ee15c08917df0c188e004f5
EC2 Default User <ec2-user@ip-172-31-70-32.ec2.internal>
2021-06-17 08:13:03 +0000

    [Finish] Rails-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'todolists/new'
+  get 'todolists/new', as: "todolist_new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "top" => "homes#top"
+  get "top" => "homes#top", as: "top"
   post "todolists" => "todolists#create"
   get "todolists" => "todolists#index"
   get "todolists/:id" => "todolists#show", as: "todolist"
   get "todolists/:id/edit" => "todolists#edit", as: "edit_todolist"
   patch "todolists/:id" => "todolists#update", as: "update_todolists"
+  delete "todolists/:id" => "todolists#destroy", as: "destroy_todolist"
 end
